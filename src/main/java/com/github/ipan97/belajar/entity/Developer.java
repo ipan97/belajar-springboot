e70c439234665d992041f20f877e2460b1d52e32
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2016-12-27 11:22:34 +0700

    fix entitas

---
+++ src/main/java/com/github/ipan97/belajar/entity/Developer.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2016 Pivotal Software, Inc..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.ipan97.belajar.entity;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author ipan97
+ */
+@Entity
+@Table(name = "developer")
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @ManyToMany
+    private List<Skill> skills;
+
+    public Developer(Long id, String firstName, String lastName, String email, List<Skill> skills) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.skills = skills;
+    }
+
+    public Developer() {
+        super();
+    }
+
+    public Developer(String firstName, String lastName, String email, List<Skill> skills) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.skills = skills;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    public Boolean hasSkill(Skill skill) {
+        for (Skill containedSkill : getSkills()) {
+            if (containedSkill.getId() == skill.getId()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/main/java/com/github/ipan97/belajar/entity/Skill.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2016 Pivotal Software, Inc..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.ipan97.belajar.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author ipan97
+ */
+@Entity
+@Table(name = "skill")
+public class Skill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "label")
+    private String label;
+    @Column(name = "description")
+    private String description;
+
+    public Skill() {
+        super();
+    }
+
+    public Skill(Long id, String label, String description) {
+        this.id = id;
+        this.label = label;
+        this.description = description;
+    }
+
+    public Skill(String label, String description) {
+        this.label = label;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
